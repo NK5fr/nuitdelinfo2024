cfc2b0b52d063fe03a470db7cb2e056eef4294c4
Raphaël Kiecken <raphael.kiecken.etu@univ-lille.fr>
2024-12-05 22:57:09 +0100

    Refactor HTML structure and CSS styles for improved layout and responsiveness

--- src/main.ts
+++ src/main.ts
@@ -2,10 +2,25 @@ import startCaptcha from "./captcha.ts";
 
 const captcha = document.querySelector('.captcha');
 
+const standardDimensions = {
+    width: 1920,    
+    height: 1080
+};
+
 startCaptcha(captcha!);
 
-const humanSchema = document.querySelector('.human-schema');
+const content = document.querySelector('.content') as HTMLDivElement;
 
-document.addEventListener('resize', () => {
-    humanSchema!.style.height = `${humanSchema!.scrollHeight}px`;
+content.style.width = '100%';
+content.style.height = '3000px';
+
+const humanSchema = document.querySelector('.human-schema') as HTMLDivElement;
+
+humanSchema.style.height = content.style.height;
+
+document.addEventListener('click', (e: MouseEvent) => {
+    const x = Math.round(e.x/standardDimensions.width * 100);
+    const y = Math.round(e.y/standardDimensions.height * 100);
+    console.log(x, y);
+    console.log(window.scrollX, window.scrollY);
 });
