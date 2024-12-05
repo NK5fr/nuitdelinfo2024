401951636d81e9d43037f989b61fa79818aa9e7e
nathan desmee <nathan.desmee.etu@univ-lille.fr>
2024-12-05 21:28:23 +0100

    shark and exit

--- src/captcha.ts
+++ src/captcha.ts
@@ -1,3 +1,6 @@
+let count = 0;
+let distance = 0;
+
 function generateCaptcha(captcha:Element) {
     let result = '';
     for (let i = 0; i < 6; i++) {
@@ -10,8 +13,75 @@ function generateCaptcha(captcha:Element) {
     captcha!.innerHTML = result;
 }
 
+function initShark(captcha:Element) {
+    const i = Math.floor(Math.random() * 6);
+    const j = Math.floor(Math.random() * 8);
+    const cell = captcha.querySelector(`.position-${i}-${j}`);
+    cell!.innerHTML = 'S';
+    cell!.classList.add('shark');
+}
+
+function initExit(captcha:Element) {
+    const shark = captcha.querySelector('.shark');
+    const x = shark!.className.split(' ')[1].split('-')[1];
+    const y = shark!.className.split(' ')[1].split('-')[2];
+    const i = Math.floor(Math.random() * 6);
+    const j = Math.floor(Math.random() * 8);
+    const cell = captcha.querySelector(`.position-${i}-${j}`);
+    cell!.innerHTML = 'E';
+    cell!.classList.add('exit');
+}
+
+function neighbors(x:number, y:number, i:number, j:number) {
+    return Math.abs(x - i) + Math.abs(y - j) === 1;
+}
+
+function move(captcha:Element, cell:Element) {
+    const shark = captcha.querySelector('.shark');
+    const i = shark!.className.split(' ')[1].split('-')[1];
+    const j = shark!.className.split(' ')[1].split('-')[2];
+    const x = cell.className.split(' ')[1].split('-')[1];
+    const y = cell.className.split(' ')[1].split('-')[2];
+    let win = false;
+    if(neighbors(parseInt(x), parseInt(y), parseInt(i), parseInt(j))) {
+        shark!.innerHTML = '';
+        shark!.classList.remove('shark');
+        if(cell.classList.contains('exit')) win = true
+        cell.innerHTML = 'S';
+        cell.classList.add('shark');
+        count++;
+    }
+    if(win){
+        captcha.setAttribute('style', 'display: none');
+    }else if(count >= distance) {
+        startCaptcha(captcha);
+    }
+};
+
+function setDistance(captcha:Element) {
+    const shark = captcha.querySelector('.shark');
+    const i = shark!.className.split(' ')[1].split('-')[1];
+    const j = shark!.className.split(' ')[1].split('-')[2];
+    const exit = captcha.querySelector('.exit');
+    const x = exit!.className.split(' ')[1].split('-')[1];
+    const y = exit!.className.split(' ')[1].split('-')[2];
+    distance = Math.abs(parseInt(x) - parseInt(i)) + Math.abs(parseInt(y) - parseInt(j));
+}
+
 const startCaptcha = (captcha:Element) => {
+    count = 0;
     generateCaptcha(captcha);
+    initShark(captcha);
+    initExit(captcha);
+    setDistance(captcha);
+    const cells = captcha.querySelectorAll('.cell');
+    cells.forEach(cell => {
+        cell.addEventListener('click', (event:Event) => {
+            event.preventDefault();
+            console.log('clicked');
+            move(captcha, cell);
+        });
+    });
 }
 
 export default startCaptcha;
